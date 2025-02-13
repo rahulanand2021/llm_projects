c437f8c7180ea449ec4c8443770c7792cb3bd079
Rahul Anand <rahulanand2006@gmail.com>
2025-02-13 16:50:36 -0500

    Added Gradio

--- gradioUIFirst.py
+++ gradioUIFirst.py
@@ -13,17 +13,149 @@ def message_gpt(user_prompt):
     ]
     
     try:
+
         completion = getOpenAI().chat.completions.create(
             model=gpt_model , # Changed from gpt-4o-mini to gpt-4
             messages=messages,
         )
         return completion.choices[0].message.content
+
     except Exception as e:
         print(f"Error making OpenAI API call: {str(e)}")
         return f"An error occurred: {str(e)}"
 
+def stream_gemini(user_prompt):
+    loadSystemPrompts()
+    prompts = [
+                {"role": "system", "content": gemini_system_message},
+                {"role": "user", "content": user_prompt}
+            ]
+    
+    stream = getGemini().chat.completions.create(
+        model=gemini_model,
+        messages=prompts,
+        stream=True
+    )
+    content=""
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            content += chunk.choices[0].delta.content
+            yield content
+    
+
+def message_claude(user_prompt):
+
+    loadSystemPrompts()
+    try:
+        message = getAnthropic().messages.create(
+                    model=claude_model,
+                    max_tokens=200,
+                    temperature=0.7,
+                    system=claude_system_message,
+                    messages=[
+                        {"role": "user", "content": user_prompt},
+                    ]   ,
+        )
+        return(message.content[0].text)
+    except Exception as e:
+        print(f"Error making Claude API call: {str(e)}")
+        return f"An error occurred: {str(e)}"
+    
+def shout(text):
+    print(f"Shout has been called with input {text}")
+    return text.upper()
+
+def stream_gpt(prompt):
+
+    loadSystemPrompts()
+    messages = [
+        {"role": "system", "content": gpt_system_message},
+        {"role": "user", "content": prompt}
+      ]
+    stream = getOpenAI().chat.completions.create(
+        model=gpt_model,
+        messages=messages,
+        stream=True
+    )
+    result = ""
+    for chunk in stream:
+        result += chunk.choices[0].delta.content or ""
+        yield result
+
+def stream_claude(prompt):
+    result = getAnthropic().messages.stream(
+        model=claude_model,
+        max_tokens=1000,
+        temperature=0.7,
+        system=claude_system_message,
+        messages=[
+            {"role": "user", "content": prompt},
+        ],
+    )
+    response = ""
+    with result as stream:
+        for text in stream.text_stream:
+            response += text or ""
+            yield response
+
+def stream_model(prompt, model):
+    if model=="GPT":
+        result = stream_gpt(prompt)
+    elif model=="Claude":
+        result = stream_claude(prompt)
+    elif model=="Gemini":
+        result = stream_gemini(prompt)
+    else:
+        raise ValueError("Unknown model")
+    yield from result
+
+
+
+def showUIGPTStream():
+     view = gr.Interface(
+            fn=stream_gpt,
+            inputs=[gr.Textbox(label="Your message:")],
+            outputs=[gr.Markdown(label="Response:")],
+            flagging_mode="never"
+            )
+     view.launch(inbrowser=True)
+
+def showUIClaudeStream():
+     view = gr.Interface(
+            fn=stream_claude,
+            inputs=[gr.Textbox(label="Your message:")],
+            outputs=[gr.Markdown(label="Response:")],
+            flagging_mode="never"
+            )
+     view.launch(inbrowser=True)
+
+def showUIWithModelSelection():
+    view = gr.Interface(
+                fn=stream_model,
+                inputs=[gr.Textbox(label="Your message:"), gr.Dropdown(["GPT", "Claude","Gemini"], label="Select model", value="GPT")],
+                outputs=[gr.Markdown(label="Response:")],
+                flagging_mode="never"
+    )   
+    view.launch(inbrowser=True)   
+
+def showUI():
+    #gr.Interface(fn=shout, inputs="textbox", outputs="textbox", flagging_mode="never").launch(share=True)
+    #gr.Interface(fn=message_gpt, inputs="textbox", outputs="textbox", flagging_mode="never").launch(inbrowser=True)
+    # view = gr.Interface(
+    #         fn=message_gemini,
+    #         inputs=[gr.Textbox(label="Your message:")],
+    #         outputs=[gr.Markdown(label="Response:")],
+    #         flagging_mode="never"
+    #         )
+    # view.launch()
+
+    # print(message_gemini("Tell me a joke !!!"))
+    # print(stream_claude("Tell me a Joke"))
+    
+    showUIWithModelSelection()
+
 if __name__ == "__main__":
     try:
-        print(message_gpt("What is the capital of France."))
+       showUI()
     except Exception as e:
         print(f"Error occurred: {e}")
--- reusable.py
+++ reusable.py
@@ -8,13 +8,17 @@ import anthropic
 # Initialize as None
 openai = None
 claude = None
+gemini = None
 
 gpt_model = "gpt-4o-mini"
 claude_model = "claude-3-haiku-20240307"
+gemini_model = "gemini-1.5-flash"
+
 gpt_system_message = "You are a helpful AI Assistant"  # Set default value
 claude_system_message = "You are a helpful AI Assistant"  # Set default value
+gemini_system_message = "You are a helpful AI Assistant"  # Set default value
 
-def loadDotenvAndCheckAPIKey():
+def loadDotenvAndCheckAPIKey(modelName):
     load_dotenv(override=True)
     openai_api_key = os.getenv('OPENAI_API_KEY')
     anthropic_api_key = os.getenv('ANTHROPIC_API_KEY')
@@ -36,13 +40,19 @@ def loadDotenvAndCheckAPIKey():
     if google_api_key:
         print(f"Google API Key exists and begins {google_api_key[:8]}")
     else:
-        print("Google API Key not set")    
-    return openai_api_key
+        print("Google API Key not set")  
+
+    if modelName == "OpenAI":  
+        return openai_api_key
+    elif modelName == "Anthropic":
+        return anthropic_api_key
+    else :
+        return google_api_key
 
 def loadOpenAI():
     """Initialize the OpenAI client and return it"""
     print("Loading OpenAI API")
-    api_key = loadDotenvAndCheckAPIKey()
+    api_key = loadDotenvAndCheckAPIKey("OpenAI")
     global openai
     openai = OpenAI(api_key=api_key)
     return openai
@@ -55,14 +65,48 @@ def getOpenAI():
     return openai
 
 def loadAnthropic():
+    """Initialize the Anthropic client and return it"""
+    print("Loading Anthropic API")
+    api_key = loadDotenvAndCheckAPIKey("Anthropic")
     global claude
-    claude = anthropic.Anthropic()
+    claude = anthropic.Anthropic(api_key=api_key)
+    return claude
+
+def getAnthropic():
+    """Get or initialize the OpenAI client"""
+    global claude
+    if claude is None:
+        claude = loadAnthropic()
+    return claude
+
+def loadGemini():
+    """Initialize the Gemini client and return it"""
+    print("Loading Gemini API")
+    api_key = loadDotenvAndCheckAPIKey("Gemini")
+    global gemini
+    # claude = anthropic.Anthropic(api_key=api_key)
+    gemini = OpenAI(
+        api_key=api_key, 
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+    )
+    return gemini
+
+def getGemini():
+    """Get or initialize the OpenAI client"""
+    global gemini
+    if gemini is None:
+        gemini = loadGemini()
+    return gemini
+
 
 def loadSystemPrompts():
     """Load system prompts with default values if not already set"""
     global gpt_system_message
     global claude_system_message
+    global gemini_system_message
     if gpt_system_message is None:
         gpt_system_message = "You are a helpful AI Assistant"
     if claude_system_message is None:
         claude_system_message = "You are a helpful AI Assistant"
+    if gemini_system_message is None:
+        gemini_system_message = "You are a helpful AI Assistant"
