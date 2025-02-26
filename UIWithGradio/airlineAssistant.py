4b314ff87096c9900ad705e4d3ed0a983caff643
Rahul Anand <rahulanand2006@gmail.com>
2025-02-25 19:19:58 -0500

    Multiple Tool Calls

--- UIWithGradio/airlineAssistant.py
+++ UIWithGradio/airlineAssistant.py
@@ -69,6 +69,11 @@ def get_ticket_price(destination_city):
     city = destination_city.lower()
     return ticket_prices.get(city, "Unknown")
 
+def book_tickets(destination_city, booking_date, price) :
+    confirmation = f" Your Flight have been booked for {destination_city} on {booking_date} for the price of {price}"
+    print(confirmation)
+    return confirmation
+
 def loadPriceFunction() :
     price_function = {
         "name": "get_ticket_price",
@@ -87,50 +92,77 @@ def loadPriceFunction() :
     }
     return price_function
 
+def loadBookingFunction() :
+    book_function = {
+        "name": "book_tickets",
+        "description": "Book the return ticket to the destination city for the given date. Call this whenever you need to book the actual tickets, for example when a customer asks 'Please book the tickets'",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "destination_city": {
+                    "type": "string",
+                    "description": "The city that the customer wants to travel to",
+                },
+                "booking_date": {
+                    "type": "string",
+                    "description": "The date that the customer wants to Book the tickets on",
+                },                
+            },
+            "required": ["destination_city","booking_date"],
+            "additionalProperties": False
+        }
+    }
+    return book_function
+
 # And this is included in a list of tools:
 def getTools():
-    tools = [{"type": "function", "function": loadPriceFunction()}]
+    tools = [{"type": "function", "function": loadPriceFunction()},
+             {"type": "function", "function": loadBookingFunction()}]
     return tools
+
+# def getTools():
+#     tools = [{"type": "function", "function": loadPriceFunction()}
+#              ]
+#     return tools
 
 def handle_tool_call(message):
     tool_call = message.tool_calls[0]
     arguments = json.loads(tool_call.function.arguments)
     city = arguments.get('destination_city')
-    price = get_ticket_price(city)
-    response = {
-        "role": "tool",
-        "content": json.dumps({"destination_city": city,"price": price}),
-        "tool_call_id": tool_call.id
-    }
+    booking_date = arguments.get('booking_date')
+    if booking_date: 
+        price = get_ticket_price(city)
+        book_tickets(city, booking_date, price)
+        response = {
+            "role": "tool",
+            "content": json.dumps({"destination_city": city,"booking_date": booking_date,"price": price}),
+            "tool_call_id": tool_call.id
+        }
+    else:
+        price = get_ticket_price(city)
+        response = {
+            "role": "tool",
+            "content": json.dumps({"destination_city": city,"price": price}),
+            "tool_call_id": tool_call.id
+        }
     return response, city
 
-def chatWithTools(history):
+def chatWithTools(message, history):
     gpt_system_message = "You are a helpful assistant for an Airline called FlightAI. "
     gpt_system_message += "Give short, courteous answers, no more than 1 sentence. "
     gpt_system_message += "Always be accurate. If you don't know the answer, say so."
 
-    messages = [{"role": "system", "content": gpt_system_message}] + history
+    messages = [{"role": "system", "content": gpt_system_message}] + history + [{"role": "user", "content": message}]
+    response = re.getOpenAI().chat.completions.create(model=re.gpt_model, messages=messages, tools=getTools())
 
-    image = None
-
-    response = re.getOpenAI().chat.completions.create(
-                model=re.gpt_model, 
-                messages=messages, 
-                tools=getTools())
-    
     if response.choices[0].finish_reason=="tool_calls":
         message = response.choices[0].message
+        print(f"Message from Tool Calls : {message}")
         response, city = handle_tool_call(message)
         messages.append(message)
         messages.append(response)
-        image = artist(city)
-        response = re.getOpenAI().chat.completions.create(
-            model=re.gpt_model, 
-            messages=messages)
-        reply = response.choices[0].message.content
-        history += [{"role":"assistant", "content":reply}]
-
-    return history, image
+        response =  re.getOpenAI().chat.completions.create(model=re.gpt_model, messages=messages)
+    return response.choices[0].message.content
 
 def artist(city):
     image_response = re.getOpenAI().images.generate(
@@ -169,11 +201,11 @@ def showCustomeUI():
 if __name__ == "__main__":
     try:
         re.loadSystemPrompts()
-        # gr.ChatInterface(fn=chatWithTools, type="messages").launch(inbrowser=True)
+        gr.ChatInterface(fn=chatWithTools, type="messages").launch(inbrowser=True)
         # image = artist("New York City")
         # plt.imshow(image)
         # plt.axis('off')  # Hide axes
         # plt.show()  # This will open a new window with the imag
-        showCustomeUI()
+        # showCustomeUI()
     except Exception as e:
         print(f"Error occurred: {e}")
